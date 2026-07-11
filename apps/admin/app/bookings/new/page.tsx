eed82d2a4870b3cdb6f6bef37b7a11d4452b9e7a
Romio Nguyen <nguyenromio@gmail.com>
2026-07-11 12:05:06 +0700

    add script clean

--- apps/admin/app/bookings/new/page.tsx
+++ apps/admin/app/bookings/new/page.tsx
@@ -4,5 +4,5 @@ import dynamic from 'next/dynamic';
 const ComponentPage = dynamic(() => import('@/containers/bookings/new'), { ssr: false });
 
 export default function Page() {
-  return <ComponentPage />;
+  return <ComponentPage tenantId="tenant-demo-01" />;
 }
